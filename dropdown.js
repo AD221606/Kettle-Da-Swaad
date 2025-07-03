1900126339da22f35404be816fe2a79842cea8ea
Aryan <aryandubey221606@gmail.com>
2025-07-03 15:17:40 +0530

    minor fixes

--- dropdown.js
+++ dropdown.js
@@ -1,11 +1,12 @@
+ //Make items visible
 function show(itemContainer) {
     itemContainer.classList.add('visible');
     itemContainer.classList.remove('invisible');
 }
-
+//Make items invisible
 function hide(itemContainer) {
     itemContainer.classList.remove('visible');
     itemContainer.classList.add('invisible');
 }
 
-export { show, hide }; 
+export { show, hide }; //branch check
